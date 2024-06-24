62fbdd220e75243fad1309dd2edb1e0ba7e5ef97
rafaelgoncalves2201 <rgcarmo545@gmail.com>
2024-06-24 17:50:00 -0300

    Fiz um SideBar de outro modelo

---
+++ SideTeste/scripts.js
@@ -0,0 +1,8 @@
+function abrirNav(){
+    document.getElementById("menu-oculto").style.width="250px"
+    document.getElementById("principal").style.left="250px"
+}
+function fecharNav(){
+    document.getElementById("menu-oculto").style.width="0px"
+    document.getElementById("principal").style.left="0px"
+}
